eace26519d1a8f5df4692212f08950d1f8d29e40
Nikolla2502 <nikolla@wanadoo.fr>
2022-04-29 11:29:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,77 @@
+
+// Affichage nav mobile
+function menuMobile() {
+    const btn = document.querySelector('.burger');
+    const header = document.querySelector('.header');
+    const links = document.querySelectorAll('.navbar a');
+
+    btn.addEventListener('click', () => {
+        header.classList.toggle('show-nav');
+    });
+    links.forEach(link => {
+        link.addEventListener("click",() => {
+            header.classList.remove('show-nav');
+        });
+    })
+}
+menuMobile();
+
+/* creation filtre projets */
+
+function tabsFilters() {
+    const tabs = document.querySelectorAll('.portfolio-filters a');
+    const projects = document.querySelectorAll('.portfolio .card');
+
+    const resetActiveLinks = () => {
+        tabs.forEach(elem => {
+            elem.classList.remove('active');
+        });
+    }
+
+    const showProjects = (elem) => {
+        // console.log(elem);
+        projects.forEach(project => {
+            let filter = project.getAttribute('data-category');
+            if (elem === 'allProject') {
+                project.parentNode.classList.remove('hide');
+                return                                          // le return arrete la fonction
+            }
+
+            // methode classique
+            if(filter !== elem) {
+                project.parentNode.classList.add('hide');
+            }else {
+                project.parentNode.classList.remove('hide');
+            }
+
+            // methode operation ternaire
+            // filter !== elem ? project.parentNode.classList.add('hide') : project.parentNode.classList.remove('hide');
+        });
+    }
+
+    tabs.forEach( elem => {
+        elem.addEventListener('click',(event) => {
+            event.preventDefault();
+            let filter = elem.getAttribute('data-filter');
+            showProjects(filter)
+            resetActiveLinks('click')
+            elem.classList.add('active');
+        });
+    })
+    
+}
+
+tabsFilters()
+
+
+// Modale carroussel
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+
+modalTriggers.forEach((trigger) =>
+  trigger.addEventListener("click", toggleModal)
+);
+
+function toggleModal() {
+  modalContainer.classList.toggle("active");
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+<img src='./images/piano_resize.jpg'>
+<h2>Portfolio de Loup Talon</h2>
+<h3>Ingenieur bois diplomé <a href='https://www.esb-campus.fr/'>ESB Nantes</a></h3>
+
+<a href='https://nikolla2502.github.io/Loup_Portfolio/' target='_blank'>Portfolio Loup Talon</a>
